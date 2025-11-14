90e89f2a0328c658dbcb4ed07d4252160df41df9
sditto <sditto28@stsaviour.org>
2025-11-14 14:59:06 -0500

    lab 3

--- game.py
+++ game.py
@@ -1,9 +1,7 @@
 import random
 import time
 
-from draw import draw_d20, draw_d6, draw_d4
-
-def print_dramatic_text(text: str, delay=0.1):
+def print_dramatic_text(text: str, delay=0.05):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
@@ -17,11 +15,32 @@ if __name__ == '__main__':
     # any critical success / failure?
 
     name = input('Name: ')
-    role = input('Role: ')
 
-    print('Your name is ' + name + ' and your role is ' + role + '.')
-    print_dramatic_text('Our adventure begins in a shady tavern ...')
+    print_dramatic_text('Welcome to Marbles!!') 
+    print_dramatic_text('In this game, you will start with 10 marbles. A number will be randomly rolled on a dice and, before seeing it, you must guess if it is odd or even.')
+    print_dramatic_text('If you correctly guess if the number is odd or even, the number rolled will be added to your marble amount')
+    print_dramatic_text('However, if you guess incorrectly, the number rolled will be taken from your marble amount.')
+    print_dramatic_text('You will win once you gain 20 marbles, but if you run out, you will lose the game.')
 
-    input('Press Enter to roll a d20.')
-    roll = random.randint(1, 20)
-    draw_d20(roll)
+
+    marbles = 10
+    while marbles < 20 and marbles > 0:
+        r = random.randint(1, 9)
+        answer = input('odd or even? ')
+        if answer == 'even' and r % 2 == 0:
+            print_dramatic_text('Correct! the number was ' + str(r))
+            marbles += r
+            print('You have ' + str(marbles) + ' marbles')
+        elif answer == 'odd' and r % 2 == 1:
+            print_dramatic_text('Correct! the number was ' + str(r))
+            marbles += r
+            print('You have ' + str(marbles) + ' marbles')
+        else:
+            print_dramatic_text('Sorry the number was ' + str(r) + ' ...')
+            marbles -= r
+            print('You have ' + str(marbles) + ' marbles')
+
+    if marbles >= 20:
+        print_dramatic_text('Congratulations! You have ' + str(marbles) + ' marbles! You win!')
+    else:
+        print_dramatic_text('Sorry -- you\'ve run out of marbles! You lose!')
